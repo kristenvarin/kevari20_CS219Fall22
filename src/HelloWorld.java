5abd7c36cf003025b2c112c48c754d41745c06c4
Kristen <kristen@varinweb.com>
2022-08-30 15:14:03 -0400

    My first Java program

--- src/HelloWorld.java
+++ src/HelloWorld.java
@@ -1,13 +1,21 @@
 // By convention, class names are capitalized.
-// A class contains functions
-// Every Java Program must have a main function
+// A class contains functions.
+// Every Java Program must have a main function.
 public class HelloWorld {
+
+    /*
+        This is a multiline block comment.
+        Another comment line.
+     */
     public static void main(String [] args) {
         System.out.println("Hello World");
 
         int x = 5;
         boolean z = true;
         double pi = 3.14159;
+        String name = "CS219 Techniques of Computer Science";
+        double avagadros = 6.022140857e23; // Scientific notation universal
+        double electron_mass = 9.1093837015e-31;
     }
 }
 
