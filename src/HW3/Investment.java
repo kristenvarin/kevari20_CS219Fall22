2d3a5c02fa4f022dd739f393835bdc910bbec855
Kristen <kristen@varinweb.com>
2022-09-15 13:08:48 -0400

    Use packages and new examples

--- src/HW3/Investment.java
+++ src/HW3/Investment.java
@@ -22,29 +22,40 @@ public class Investment {
         }
 
     System.out.print("Enter a yearly interest rate as a decimal: ");
-    double rate=kbd.nextDouble();
-                    if(rate< 0||rate>1){
-        System.out.printf("Error: Please enter a positive decimal number between 0.0 and 1.0. "+
-        "You entered \"%s\"",rate);
-        System.exit(-1);
+        if (!kbd.hasNextDouble()) {
+            System.out.printf("Error: Please enter a decimal number. You entered \"%s\"", kbd.next());
+            System.exit(-1);
+        }
+        double rate=kbd.nextDouble();
+            if(rate< 0||rate>1){
+            System.out.printf("Error: Please enter a positive decimal number between 0.0 and 1.0. "+
+            "You entered \"%s\"",rate);
+            System.exit(-1);
         }
 
         System.out.print("Enter a number of years as a positive integer: ");
+        if (!kbd.hasNextDouble()) {
+            System.out.printf("Error: Please enter a decimal number. You entered \"%s\"", kbd.next());
+            System.exit(-1);
+        }
         double years=kbd.nextDouble();
-        if(years< 0){
-        System.out.printf("Error: Please enter a positive integer."+
-        "You entered \"%s\"",years);
-        System.exit(-1);
+            if(years< 0){
+            System.out.printf("Error: Please enter a positive integer."+
+            "You entered \"%s\"",years);
+            System.exit(-1);
         }
 
         System.out.print("Enter a compound interest as a positive decimal: ");
-        double interest=kbd.nextDouble();
-        if(interest< 0){
-        System.out.printf("Error: Please enter a positive decimal number between 0.0 and 1.0. "+
-        "You entered \"%s\"",interest);
-        System.exit(-1);
+        if (!kbd.hasNextDouble()) {
+            System.out.printf("Error: Please enter a decimal number. You entered \"%s\"", kbd.next());
+            System.exit(-1);
         }
-
+        double interest=kbd.nextDouble();
+            if(interest< 0){
+            System.out.printf("Error: Please enter a positive decimal number between 0.0 and 1.0. "+
+            "You entered \"%s\"",interest);
+            System.exit(-1);
+        }
 
         System.out.print("Your return on investment is ");
         System.out.println(investment(deposit,rate,years,interest));
